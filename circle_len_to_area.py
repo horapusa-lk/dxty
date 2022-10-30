c4d5bad770e3555afa35f1accea84ad0f598b92a
Bhathiya Gamage <73831309+horapusa-lk@users.noreply.github.com>
2022-10-30 13:51:11 +0530

    Create circle_len_to_area.py

---
+++ circle_len_to_area.py
@@ -0,0 +1,6 @@
+circle_len = int(input("Enter circle len (cm) : "))
+x = circle_len / 2
+y = x / 3.141592653589793238
+a = y ** 2
+circle_area = a * 3.141592653589793238
+print(f"Circle area is : {circle_area} cm³")
